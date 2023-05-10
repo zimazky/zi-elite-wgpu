d865d46ab288801aebbdb53ad5f2c015f637980a
andrew zimatskiy <zimazky@gmail.com>
2023-05-10 21:18:40 +0300

    Два треугольника с разными биндинг-группами

---
+++ src/view/webgpu.md
@@ -0,0 +1,103 @@
+```
+pipeline
+  vertex - vertex state
+    module - shader module
+    entryPoint - function in shader
+    buffers - array of vertex buffer layouts
+      arrayStride - stride in bytes between elenents of buffer array
+      attributes - array defining the layout of vertex attributes
+        shaderLocation - number of location
+        format - type of attribute
+        offset - offset in bytes from the beginning of element
+  fragment - fragment state
+    module - shader module
+    entryPoint - function in shader
+    targets - array of color target states (may multiple render targets)
+      format - color target
+  primitive - primitive state
+    topology - points, lines, triangles in variations
+  depthStencil
+    format - "depth24plus"
+    depthWriteEnabled
+    depthCompare - "less"
+
+
+  layout - pipeline layout
+
+
+buffer descriptor
+  size - количество элементов вершинного буфера
+  usage - битовая маска для определения использования буфера (вершинный или индексный и доступ)
+  mappedAtCreation - если true, то создается буфер отображаемом на локальную память состоянии
+  
+texture descriptor
+  size
+    width
+    height
+  format - формат цвета текстуры
+  uasge - битовая маска определяющая использование текстуры
+      
+?texture view descriptor
+  format - формат цвета текстуры
+  dimension - размерность текстуры
+  aspect - 'all', 'depth-only', 'ctencil-only'
+  baseMipLevel - первый, самый детальный уровень mip
+  mipLevelCount - число уровней mip
+  ?baseArrayLayer - индекс первого слоя массива
+  ?arrayLayerCount - число слоев
+
+sampler descriptor
+  addressModeU - тип выборки за краем текстуры в направлении U: 'repeat', 'clamp-to-edge', 'mirror-repeat'
+  addressModeV - тип выборки за краем текстуры в направлении V
+  magFilter - фильтрация: 'linear', 'nearest'
+  minFilter - фильтрация
+  mipmapFilter - выбор mip уровня: 'nearest', 'linear'
+  maxAnisotropy: - уровень анизотропной фильтрации: 1-16
+  
+
+
+pipeline layout descriptor
+  bindingGroupLayouts - array of binding group layouts
+
+
+binding group layout descriptor
+  entries - array
+    binding - number of binding
+    visibility - bitset of stages: vertex, fragment, compute
+    buffer: {} - indicate type of binding
+    texture: {} - indicate type of binding
+    sampler: {} - indicate type of binding
+
+
+binding group descriptor
+  ?layout - 
+  entries - array
+    binding - number of binding
+    resource - may be: sampler, texture view, external texture, buffer binding (uniform)
+      buffer
+      offset
+      size
+
+
+renderpass descriptor
+  colorAttachments - массив
+    view - texture view
+    clearValue - цвет очищения 
+     r
+     g
+     b
+     a
+    loadOp - начальная операция: 'clear', 'load'
+    storeOp - операция сохранения: 'store', 'discard'
+  depthStencilAttachment
+    view: depthTexture.createView(),
+    depthClearValue: 1.0,
+    depthLoadOp: 'clear',
+    depthStoreOp: "store",
+    /*stencilClearValue: 0,
+    stencilStoreOp: "store",
+    depthLoadOp: 'clear',
+    stencilLoadOp: 'clear'*/
+
+
+```
--- src/view/webgpu.ts
+++ src/view/webgpu.ts
@@ -1,11 +1,11 @@
 import { loadImg } from '../utils/loadimg';
 import { Material } from './material';
-import { toRad } from '../utils/mathutils';
 import { Triangle } from './triangle';
-import { Mat4, Vec3 } from '../utils/vectors';
 import shader from '/src/shaders/shader.wgsl';
 import { Camera } from 'src/model/camera';
 import { ModelA } from 'src/model/modelA';
+
+const uniformOffset = 256;  // выравнивание буфера для разных биндинг-групп
 
 export class WebGPU {
   private format: GPUTextureFormat = 'bgra8unorm';
@@ -16,7 +16,7 @@ export class WebGPU {
 
   uniformBuffer: GPUBuffer;
   pipeline: GPURenderPipeline;
-  bindGroup: GPUBindGroup;
+  bindGroups: GPUBindGroup[] = []; // группы для разных треугольников
   renderPassDescriptor: GPURenderPassDescriptor;
   triangle: Triangle;
   material: Material;
@@ -40,8 +40,11 @@ export class WebGPU {
     const img = await loadImg('textures/cat-head.jpg');
     this.material.initialize(this.device,img);
 
+    const uniformSize = 3*4*16; // две матрицы преобразования
+    const uniformBufferSize = uniformOffset + uniformSize; // общий размер буфера
+
     this.uniformBuffer = this.device.createBuffer({
-      size: 64*3,
+      size: uniformBufferSize,
       usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST
     });
 
@@ -52,10 +55,28 @@ export class WebGPU {
         {binding: 2, visibility: GPUShaderStage.FRAGMENT, sampler: {}}
       ]
     });
-    this.bindGroup = this.device.createBindGroup({
+    // группа для первого треугольника
+    this.bindGroups[0] = this.device.createBindGroup({
       layout: bindGroupLayout,
       entries: [
-        {binding: 0, resource: {buffer: this.uniformBuffer}},
+        {binding: 0, resource: {
+          buffer: this.uniformBuffer,
+          offset: 0,
+          size: uniformSize
+        }},
+        {binding: 1, resource: this.material.view},
+        {binding: 2, resource: this.material.sampler}
+      ]
+    });
+    // группа для второго треугольника
+    this.bindGroups[1] = this.device.createBindGroup({
+      layout: bindGroupLayout,
+      entries: [
+        {binding: 0, resource: {
+          buffer: this.uniformBuffer,
+          offset: uniformOffset,
+          size: uniformSize
+        }},
         {binding: 1, resource: this.material.view},
         {binding: 2, resource: this.material.sampler}
       ]
@@ -99,9 +120,6 @@ export class WebGPU {
     //const view = Mat4.lookAt(new Vec3(0,2,2), new Vec3(0.5,0,0), Vec3.J()) //camera.transform;
     const view = camera.transform.copy();
 
-    this.device.queue.writeBuffer(this.uniformBuffer, 64, new Float32Array(view.getArray()));
-    this.device.queue.writeBuffer(this.uniformBuffer, 128, new Float32Array(projection.getArray()));
-
     const textureView = this.context.getCurrentTexture().createView();
     const commandEncoder = this.device.createCommandEncoder();
     const renderpass = commandEncoder.beginRenderPass({
@@ -115,10 +133,12 @@ export class WebGPU {
     renderpass.setPipeline(this.pipeline);
     renderpass.setVertexBuffer(0, this.triangle.buffer);
 
-    triangles.forEach(t=>{
+    triangles.forEach((t,i)=>{
       const model = t.transform.copy();
-      this.device.queue.writeBuffer(this.uniformBuffer, 0, new Float32Array(model.getArray()));
-      renderpass.setBindGroup(0, this.bindGroup);
+      this.device.queue.writeBuffer(this.uniformBuffer, i*uniformOffset, new Float32Array(model.getArray()));
+      this.device.queue.writeBuffer(this.uniformBuffer, i*uniformOffset + 64, new Float32Array(view.getArray()));
+      this.device.queue.writeBuffer(this.uniformBuffer, i*uniformOffset + 128, new Float32Array(projection.getArray()));
+      renderpass.setBindGroup(0, this.bindGroups[i]);
       renderpass.draw(3, 1, 0, 0);
       //console.log(t);
     })
